13b1927b8db031e0c97d6022964e818b9dacb780
no-merit <kewer9024@ajou.ac.kr>
2025-05-08 13:13:49 +0900

    Chore: ui-service 생성 및 초기설정

---
+++ UI-service/src/main/java/com/goormplay/uiservice/UiServiceApplication.java
@@ -0,0 +1,15 @@
+package com.goormplay.uiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class UiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiServiceApplication.class, args);
+    }
+
+}
