3b024f587e91b8e1ed1a32b1e9ad08527305fd2b
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-05-12 10:42:49 +0900

    add user interaction service for test

---
+++ userAction-test-service/src/test/java/com/goormplay/useractiontestservice/UserActionTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormplay.useractiontestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActionTestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
