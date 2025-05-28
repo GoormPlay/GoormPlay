f8318f30fe11dd8a3af5b6d304348704852d894a
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-05-28 15:35:52 +0900

    fix: modify kafka server

--- userAction-test-service/src/main/java/com/goormplay/useractiontestservice/dto/ClickEventLogDto.java
+++ userAction-test-service/src/main/java/com/goormplay/useractiontestservice/dto/ClickEventLogDto.java
@@ -1,6 +1,5 @@
 package com.goormplay.useractiontestservice.dto;
 
-import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,14 +9,13 @@ import java.util.List;
 
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class ClickEventLogDto {
-    @Nullable
     private String userId;
     private String contentId;
     private String timestamp;
-    private String eventType;             // "content_click" 또는 "content_recom_click"
-    private String page;                  // ex: "content_detail"
-    private List<String> contentCategory; // ← 여기 genre 그대로 담음
+    private String eventType;
+    private List<String> contentCategory;
+    private String page;
 }
---
+++ userAction-test-service/src/main/java/com/goormplay/useractiontestservice/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,40 @@
+package com.goormplay.useractiontestservice.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "3.37.147.123:9092,3.36.188.73:9092,54.180.180.120:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "indexing-group2");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+}
