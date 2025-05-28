6cf394b53160d338cf6f109a5b2c46b68842bfd9
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-05-28 16:17:24 +0900

    fix: add kafka variables

--- userAction-test-service/src/main/java/com/goormplay/useractiontestservice/kafka/KafkaConsumerConfig.java
+++ userAction-test-service/src/main/java/com/goormplay/useractiontestservice/kafka/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package com.goormplay.useractiontestservice.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,10 +17,12 @@ import java.util.Map;
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "3.37.147.123:9092,3.36.188.73:9092,54.180.180.120:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "indexing-group2");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
